e93c65f94156fcc889dbdc31421cefab08fd2da2
Tom1k <tomascosta@ua.pt>
2020-04-08 23:04:47 +0100

    Calling search method in controller

--- src/main/java/com/tom1k/airquality/Backend/DatabaseLoader.java
+++ src/main/java/com/tom1k/airquality/Backend/DatabaseLoader.java
@@ -1,5 +1,10 @@
 package com.tom1k.airquality.Backend;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -17,6 +22,7 @@ public class DatabaseLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         this.repository.save(new CityAir("Aveiro",39,21,"pm25"));
+        fetchCityData("porto");
     }
 
     public CityAir getCityDetails(String name){
@@ -24,12 +30,25 @@ public class DatabaseLoader implements CommandLineRunner {
         if(city!=null){
             return city;
         } else {
-            fetchCityData(name);
+            try {
+                fetchCityData(name);
+            } catch (IOException e){
+                System.out.println(e);
+            }
             return this.repository.findByName(name);
         }
     }
 
-    public void fetchCityData(String name){
-        return;
+    public String fetchCityData(String name) throws IOException {
+        URL url = new URL("https://api.waqi.info/feed/beijing/?token=76cd24d48b0fd37719669178c449821326315463");
+        URLConnection connection = url.openConnection();
+
+        try (BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream())))
+        {
+            String line = in.readLine();
+            System.out.println(line);
+            return line;
+        }
     }
 }
--- src/main/java/com/tom1k/airquality/Frontend/HomeController.java
+++ src/main/java/com/tom1k/airquality/Frontend/HomeController.java
@@ -1,14 +1,24 @@
 package com.tom1k.airquality.Frontend;
 
+import com.tom1k.airquality.Backend.DatabaseLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 public class HomeController {
 
+    private DatabaseLoader cityService;
+
     @RequestMapping(value = "/")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping(value = "/search")
+    public String search() throws IOException {
+        return this.cityService.fetchCityData("porto");
     }
 
 }
