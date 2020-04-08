2de50bf20a93e8e7c4cf926fd8a3139a560f63a6
Tom1k <tomascosta@ua.pt>
2020-04-09 00:17:30 +0100

    added baseline test

---
+++ src/test/java/com/tom1k/airquality/CityAirRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.tom1k.airquality;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityAirRepositoryTest {
+
+    @Test
+    void findByName() {
+    }
+}
---
+++ src/test/java/com/tom1k/airquality/DatabaseLoaderTest.java
@@ -0,0 +1,16 @@
+package com.tom1k.airquality;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseLoaderTest {
+
+    @Test
+    void getCityDetails() {
+    }
+
+    @Test
+    void fetchCityData() {
+    }
+}
