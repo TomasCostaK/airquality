4e5fe87e8f89a5382432c712f3b503c96c497ae8
Tom1k <tomascosta@ua.pt>
2020-04-08 22:39:36 +0100

    finalizing architecture on repo-service interaction

--- src/main/java/com/tom1k/airquality/Backend/DatabaseLoader.java
+++ src/main/java/com/tom1k/airquality/Backend/DatabaseLoader.java
@@ -16,8 +16,20 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        this.repository.save(new CityAir("Aveiro",39,21));
+        this.repository.save(new CityAir("Aveiro",39,21,"pm25"));
     }
 
+    public CityAir getCityDetails(String name){
+        CityAir city = this.repository.findByName(name);
+        if(city!=null){
+            return city;
+        } else {
+            fetchCityData(name);
+            return this.repository.findByName(name);
+        }
+    }
 
+    public void fetchCityData(String name){
+        return;
+    }
 }
