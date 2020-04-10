60c13476640e4120099fc067e7d4410e521e5458
Tom1k <tomascosta@ua.pt>
2020-04-11 00:40:27 +0100

    finished DAO Implementation

--- SpringBoot/airquality/src/main/java/com/tom1k/airquality/CityDAO.java
+++ SpringBoot/airquality/src/main/java/com/tom1k/airquality/CityDAO.java
@@ -1,4 +1,15 @@
 package com.tom1k.airquality;
 
+import java.util.List;
+
 public interface CityDAO {
+    List<City> get();
+
+    City get(int id);
+
+    City get(String name);
+
+    void save(City city);
+
+    void delete(int id);
 }
--- SpringBoot/airquality/src/main/java/com/tom1k/airquality/CityDAOImpl.java
+++ SpringBoot/airquality/src/main/java/com/tom1k/airquality/CityDAOImpl.java
@@ -1,4 +1,52 @@
 package com.tom1k.airquality;
 
-public class CityDAOImpl {
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CityDAOImpl implements CityDAO {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public List<City> get() {
+        Session currSession = entityManager.unwrap(Session.class);
+        Query<City> query = currSession.createQuery("from City",City.class);
+        List<City> list = query.getResultList();
+        return list;
+    }
+
+    @Override
+    public City get(int id) {
+        Session currSession = entityManager.unwrap(Session.class);
+        City city = currSession.get(City.class,id);
+        return city;
+    }
+
+    // Custom get city from the name on it, can be repeated so get first?
+    @Override
+    public City get(String name) {
+        Session currSession = entityManager.unwrap(Session.class);
+        Query<City> query = currSession.createQuery("select " + name + "from City",City.class);
+        City city = query.getSingleResult();
+        return city;
+    }
+
+    @Override
+    public void save(City city) {
+        Session currSession = entityManager.unwrap(Session.class);
+        currSession.saveOrUpdate(city);
+    }
+
+    @Override
+    public void delete(int id) {
+        Session currSession = entityManager.unwrap(Session.class);
+        City city = currSession.get(City.class,id);
+        currSession.delete(city);
+    }
 }
