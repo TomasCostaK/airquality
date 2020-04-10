0d22a2ab0dab9314ce2370627b5506589f70baea
Tom1k <tomascosta@ua.pt>
2020-04-10 23:43:39 +0100

    Created City entity

--- SpringBoot/airquality/src/main/java/com/tom1k/airquality/City.java
+++ SpringBoot/airquality/src/main/java/com/tom1k/airquality/City.java
@@ -1,4 +1,92 @@
 package com.tom1k.airquality;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_city")
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int aqi;
+
+    @Column
+    private int pm10;
+
+    @Column
+    private int pm25;
+
+    @Column
+    private String dominentpol;
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", aqi=" + aqi +
+                ", pm10=" + pm10 +
+                ", pm25=" + pm25 +
+                ", dominentpol='" + dominentpol + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAqi() {
+        return aqi;
+    }
+
+    public void setAqi(int aqi) {
+        this.aqi = aqi;
+    }
+
+    public int getPm10() {
+        return pm10;
+    }
+
+    public void setPm10(int pm10) {
+        this.pm10 = pm10;
+    }
+
+    public int getPm25() {
+        return pm25;
+    }
+
+    public void setPm25(int pm25) {
+        this.pm25 = pm25;
+    }
+
+    public String getDominentpol() {
+        return dominentpol;
+    }
+
+    public void setDominentpol(String dominentpol) {
+        this.dominentpol = dominentpol;
+    }
 }
