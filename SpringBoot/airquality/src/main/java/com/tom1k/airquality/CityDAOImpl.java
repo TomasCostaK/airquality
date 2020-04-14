5f492cd4c2bf78efe525ce5fa9e568ed7fe33bc2
Tom1k <tomascosta@ua.pt>
2020-04-14 16:49:21 +0100

    fetch success for cities on in db

--- SpringBoot/airquality/src/main/java/com/tom1k/airquality/CityDAOImpl.java
+++ SpringBoot/airquality/src/main/java/com/tom1k/airquality/CityDAOImpl.java
@@ -1,5 +1,4 @@
 package com.tom1k.airquality;
-
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
@@ -9,8 +8,20 @@ import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Repository
 public class CityDAOImpl implements CityDAO {
+
+    private static final String GET_URL = "https://api.waqi.info/feed";
+    private static final String USER_AGENT = "Mozilla/5.0";
+    //Have this token removed later, so people are not making use of my API
+    private static final String TOKEN = "76cd24d48b0fd37719669178c449821326315463";
 
     @Autowired
     private EntityManager entityManager;
@@ -45,9 +56,47 @@ public class CityDAOImpl implements CityDAO {
             System.out.println(e);
             System.out.println("City not on Cache, fetching.");
 
+            //If the code didnt have in cache, we must run a call for our api and then return the city we have created
+            try {
+                sendGET(name);
+            }catch (IOException e2){
+                System.out.println(e2);
+            }
+
+
+            //The database should clear itself to keep fewer items in cache
         }
         City city = new City();
         return city;
+    }
+
+    private static void sendGET(String name) throws IOException {
+        String URL_FINAL = GET_URL + "/" + name + "/?token=" + TOKEN;
+        System.out.println(URL_FINAL);
+        URL obj = new URL(URL_FINAL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println("" + response["data"])
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request didnt work");
+        }
+
     }
 
     @Override
